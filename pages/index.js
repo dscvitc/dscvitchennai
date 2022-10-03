1397c3e3d06e8e611a0523d22dbddc2f670c5e1b
Kaushal Barhate <barhate.kaushal@gmail.com>
2022-10-03 19:48:51 +0530

    Main

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
-import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Index from "../components/Index";
 
