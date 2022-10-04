c01b33750eb85fbd7b7c32efddd38d6607c2151c
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-10-04 11:35:49 +0530

    Lint Fix: Added key to list

--- pages/contributors.js
+++ pages/contributors.js
@@ -1,9 +1,11 @@
+import CardList from "../components/contributors/List";
 import Navbar from "../components/Navbar";
 
 const Contributors = () => {
   return (
     <>
       <Navbar />
+      <CardList />
     </>
   );
 };
