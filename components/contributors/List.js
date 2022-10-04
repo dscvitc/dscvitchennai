bf29b4f17a9c8a1c18ca4c26c1f1617e9503d85d
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-10-04 14:00:36 +0530

    [UI]: Added Contributors List heading

--- components/contributors/List.js
+++ components/contributors/List.js
@@ -1,10 +1,16 @@
 import Link from "next/link";
 import ContributorList from "../../data/contributors.json";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const cardVariants = {};
 
 const CardItem = ({ item }) => {
   return (
-    <div className="bg-slate-100 flex flex-col justify-evenly items-center h-80 rounded-lg shadow-lg">
+    <motion.div
+      whileHover={{ scale: 1.05 }}
+      className="bg-slate-100 flex flex-col justify-evenly items-center h-80 rounded-lg shadow-lg"
+    >
       <div className="w-36 h-36 rounded-full overflow-hidden">
         <img
           src={item.image !== null ? item.image : "/assets/avatar.jpg"}
@@ -24,14 +30,17 @@ const CardItem = ({ item }) => {
           <FaLinkedin className="cursor-pointer" size={24} />
         </Link>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
 export default function CardList() {
   return (
     <section className="max-w-7xl mx-auto">
-      <div className="grid grid-cols-3 gap-5 mx-5 my-5">
+      <h1 className="my-10 text-center text-2xl font-semibold">
+        Contributors List
+      </h1>
+      <div className="grid grid-cols-3 gap-5">
         {ContributorList.map((item, index) => (
           <CardItem key={index} item={item} />
         ))}
