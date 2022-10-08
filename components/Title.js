dba0b20e3609eb90bb3673829d7be06538ab4e0e
VIJAY CHAUHAN <mr.vijaychauhan08@gmail.com>
2022-10-08 19:13:36 +0530

    Create Home Section and Vision Section

---
+++ components/Title.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import DownArrow from "../Assets/Icons/down-arrow.png";
+
+const Title = ({ title }) => {
+  return (
+    <div className="flex flex-col items-center justify-center ">
+      <Image src={DownArrow} width={30} height={30} className="pb-5" />
+      <h1 className="font-semibold text-[22px]">{title}</h1>
+    </div>
+  );
+};
+export default Title;
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import Link from "next/link";
 import { useState } from "react";
 import Navbar from "../components/Navbar";
 import Index from "../components/Index";
+import Vision from "../components/Vision";
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Index />
+      <Vision />
     </div>
   );
 }
