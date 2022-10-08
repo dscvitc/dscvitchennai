260a90bd8556c79f3378148ed79a72aea3ec5c2a
Mohammad Shaad Shaikh <shaikh.mohammad2021@vitstudent.ac.in>
2022-10-08 20:14:56 +0530

    resolved errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      'Rich Black': '#040404',
-      'Silver': '#C1C1C1',
-      'Cultured': '#F6F6F6',
-      'White': '#FFFFFF',
-      'Azure': '#4285F4',
-      'Mango': '#FCBC04',
-      'Green Pigment': '#34A853',
-      'Cinnabar': '#EB4335',
+    extend: {
+      colors: {
+        "Rich Black": "#040404",
+        Silver: "#C1C1C1",
+        Cultured: "#F6F6F6",
+        White: "#FFFFFF",
+        Azure: "#4285F4",
+        Mango: "#FCBC04",
+        "Green Pigment": "#34A853",
+        Cinnabar: "#EB4335",
+      },
     },
   },
   plugins: [],
