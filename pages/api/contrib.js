f3b88f7dbfc7ecbdb52f3333518796c828da4f26
iampoppyxx <srihari.unnikrishnan@gmail.com>
2022-10-09 14:45:53 +0530

    Add API endpoint for /api/contrib ref. issue #35

---
+++ pages/api/contrib.js
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// Using the octokit API to get list of contributors to the Repo
+// /api/contrib
+
+import process from "process";
+const { Octokit } = require("@octokit/rest");
+
+const api_key = process.env.API_URL;
+const octokit = new Octokit({
+  auth: api_key,
+});
+
+export default function handler(req, res) {
+  const data = async () => {
+    const got = await octokit.rest.repos.listContributors({
+      owner: "dscvitc",
+      repo: "dscvitchennai",
+    });
+    res.status(200).json(got["data"]);
+  };
+  data();
+}
