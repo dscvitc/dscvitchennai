5b3189a666010f9168c84a662d3ab43059300117
Jaydeep <jaydeep.dey03@gmail.com>
2022-10-18 10:16:42 +0530

    Added Board Member card with swiperjs

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,13 @@ import Index from "../components/Index";
 import Vision from "../components/Vision";
 import Footer from "../components/Footer";
 import Boardmembers from "../components/Boardmembers";
-import Events from "../components/Events";
 export default function Home() {
   return (
     <div>
-      {/* <div className="sticky top-0"> */}
       <Navbar />
-      {/* </div> */}
       <Index />
       <Vision />
       <Boardmembers />
-      <Events />
       <Footer />
     </div>
   );
