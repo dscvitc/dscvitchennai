4b0b3a45a399776f8efb8744bec5722bf07df319
Mohammad Shaad Shaikh <shaikh.mohammad2021@vitstudent.ac.in>
2023-01-15 13:33:04 +0530

    Added animation on scroll feature (#89)
    Co-authored-by: Mohammad Shaad Shaikh <mohammadshaadshaikh@Shaad-MacBook-Pro.local>

--- components/Title.js
+++ components/Title.js
@@ -1,6 +1,7 @@
 const Title = ({ title }) => {
   return (
-    <div className="flex flex-col items-center justify-center ">
+    <div className="flex flex-col items-center justify-center " data-aos="fade-up"
+    data-aos-anchor-placement="center-center">
       <h1 className="font-bold text-5xl tracking-widest">{title}</h1>
     </div>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -15,12 +15,20 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet"
         />
+
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+
         <meta name="description" content="GDSC VIT Chennai" />
         <link rel="icon" href="gdsc.png" />
       </Head>
       <body className="bg-gray-50">
         {/* <Navbar /> */}
         <NextScript />
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+        <script>
+          AOS.init();
+        </script>
       </body>
     </Html>
   );
