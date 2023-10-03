2ff30a918e37f1695626943040202b061ca56d57
Aditya <aj.adityajyoti@gmail.com>
2023-10-03 14:23:41 +0530

    Update board members and store archive data

--- data/BoardMember.js
+++ data/BoardMember.js
@@ -1,107 +1,179 @@
-import B_lead from "../public/Leads/blockchain.webp";
-import CP_Lead from "../public/Leads/manas.jpg";
-import Marketing_lead from "../public/Leads/vigya.jpg";
-import Design_lead from "../public/Leads/design.webp";
-import DS_lead from "../public/Leads/aron.jpg";
-import OS_lead from "../public/Leads/oslead.png";
-import Overall_lead from "../public/Leads/harisha.jpg";
-import Web1_lead from "../public/Leads/arjun.jpg";
-import Web2_lead from "../public/Leads/rahul.jpg";
-import App_lead from "../public/Leads/vishal.jpg";
+import OpenSource_Lead from "../public/Leads/OpenSource.jpg";
+import CompetitiveProgramming_Lead1 from "../public/Leads/CompetitiveProgramming1.jpg";
+import CompetitiveProgramming_Lead2 from "../public/Leads/CompetitiveProgramming2.jpg";
+import SocialMedia_Lead1 from "../public/Leads/SocialMedia1.jpg";
+import SocialMedia_Lead2 from "../public/Leads/SocialMedia2.jpg";
+import Management_Lead from "../public/Leads/Management.jpg";
+import DataScience_Lead from "../public/Leads/DataScience.jpg";
+import Design_Lead from "../public/Leads/Design.jpg";
+import WebDevelopment_Lead from "../public/Leads/WebDevelopment.jpg";
+import Marketing_Lead from "../public/Leads/Marketing.jpg";
+import Placeholder from "../public/Leads/placeholder.png";
 
 const boardMembers = [
   {
-    name: "Fidal Mathew",
-    image: OS_lead,
+    name: "Zahran Sajid",
+    image: OpenSource_Lead,
     role: "Open Source Lead",
-    description: "Web Developer | Learning :)",
-    github: "https://github.com/FidalMathew",
-    linkedIn: "https://www.linkedin.com/in/fidalmathew/",
+    description: "Placeholder",
+    github: "https://github.com/zahransajid",
+    linkedIn: "https://www.linkedin.com/in/zahran-sajid/",
     color: "#4b916c",
   },
   {
-    name: "Harisha S",
-    image: Overall_lead,
-    role: "Overall Lead",
-    description: "Web Developer | Learning :)",
-    github: "https://github.com/FidalMathew",
-    linkedIn: "https://www.linkedin.com/in/fidalmathew/",
+    name: "Achintya Singh",
+    image: CompetitiveProgramming_Lead1,
+    role: "Competitive Programming Lead",
+    description:
+      "It's not what happens to you, but how you react to it that matters",
+    github: "https://github.com/AchintyaSingh0220",
+    linkedIn: "https://www.linkedin.com/in/achintya-singh-367453244/",
     color: "#4b916c",
   },
   {
-    name: "Aron Ritesh",
-    image: DS_lead,
+    name: "Jeevan Alexen Kavalam",
+    image: CompetitiveProgramming_Lead2,
+    role: "Competitive Programming Lead",
+    description: "Making problem solving fun",
+    github: "https://github.com/castlercodes",
+    linkedIn: "https://www.linkedin.com/in/jeevan-alexen-kavalam-83098a215",
+    color: "#4b916c",
+  },
+  {
+    name: "Sparsh Chakraborty",
+    image: SocialMedia_Lead1,
+    role: "Social Media Lead",
+    description: "Doom Scrolling reels is my fav hobby",
+    github: "https://github.com/codexx07",
+    linkedIn: "https://www.linkedin.com/in/sparsh-chakraborty/",
+    color: "#4b916c",
+  },
+  {
+    name: "Amara J",
+    image: SocialMedia_Lead2,
+    role: "Social Media Lead",
+    description: "One track mind, one track heart",
+    github: "https://github.com/amarafray",
+    linkedIn: "",
+    color: "#121212",
+  },
+  {
+    name: "Syed Omar Albeez",
+    image: Management_Lead,
+    role: "Management Lead",
+    description: "One who juggles with deadlines and tasks like a pro",
+    github: "https://github.com/syedomar1",
+    linkedIn: "https://www.linkedin.com/in/syed-omar-albeez/",
+    color: "#4287f5",
+  },
+  {
+    name: "Jaskaran singh walia",
+    image: DataScience_Lead,
     role: "Data Science Lead",
-    description: "Web Developer | Learning :)",
-    github: "https://github.com/FidalMathew",
-    linkedIn: "https://www.linkedin.com/in/fidalmathew/",
-    color: "#c01e34",
-  },
-
-  {
-    name: "Manas Tiwari",
-    image: CP_Lead,
-    role: "CP Lead",
-    description: "Web Developer | Learning :)",
-    github: "https://github.com/FidalMathew",
-    linkedIn: "https://www.linkedin.com/in/fidalmathew/",
-    color: "#4b916c",
+    description: "Turning caffeine and curiosity into coding masterpieces",
+    github: "https://github.com/karanwxliaa",
+    linkedIn: "https://www.linkedin.com/in/jaskaran-singh-walia-657696271/",
+    color: "#02D8E9",
   },
   {
-    name: "Vishal Sankar",
-    image: App_lead,
-    role: "App dev Lead",
-    description: "Web Developer | Learning :)",
-    github: "https://github.com/FidalMathew",
-    linkedIn: "https://www.linkedin.com/in/fidalmathew/",
-    color: "#4b916c",
-  },
-  {
-    name: "Arjun P",
-    image: Web1_lead,
-    role: "Web dev Lead",
-    description: "Web Developer | Learning :)",
-    github: "https://github.com/FidalMathew",
-    linkedIn: "https://www.linkedin.com/in/fidalmathew/",
-    color: "#4b916c",
-  },
-
-  {
-    name: "Rahul Sunov",
-    image: Web2_lead,
-    role: "Web dev Lead",
-    description: "Web Developer | Learning :)",
-    github: "https://github.com/FidalMathew",
-    linkedIn: "https://www.linkedin.com/in/fidalmathew/",
-    color: "#4b916c",
-  },
-
-  {
-    name: "Apoorva Waman",
-    image: Design_lead,
+    name: "Anurag Tamhankar",
+    image: Design_Lead,
     role: "Design Lead",
-    description: "Web Developer | Learning :)",
-    github: "https://github.com/FidalMathew",
-    linkedIn: "https://www.linkedin.com/in/fidalmathew/",
-    color: "#4b916c",
+    description:
+      "You do not rise to the level of your goals. You fall to the level of your systems",
+    github: "https://github.com/void-anurag",
+    linkedIn: "https://www.linkedin.com/in/anurag-tamhankar-75a471220/",
+    color: "#8D57FF",
   },
   {
-    name: "Narayan Subramanian",
-    image: B_lead,
-    role: "Blockchain Lead",
-    description: "Enthusiastic Web Developer",
-    github: "",
-    linkedIn: "https://www.linkedin.com/in/narayan-subramanian/",
-    color: "#4b916c",
+    name: "Vaibhav T S",
+    image: WebDevelopment_Lead,
+    role: "Web Development Lead",
+    description: "Placeholder",
+    github: "https://github.com/vaibhavTekk",
+    linkedIn: "https://www.linkedin.com/in/vaibhav-tekkalur/",
+    color: "#3d649a",
   },
   {
-    name: "Vigya Sharma",
-    image: Marketing_lead,
+    name: "Naveen Shankar Choudhury",
+    image: Marketing_Lead,
     role: "Marketing Lead",
-    description: "Web Developer | Learning :)",
+    description: "Fly High",
+    github: "https://github.com/naveenchoudhury",
+    linkedIn: "www.linkedin.com/in/naveen-shankar-choudhury-481262238",
+    color: "#4248f5",
+  },
+  {
+    name: "Aditi Jha",
+    image: Placeholder,
+    role: "Overall Lead",
+    description: "Placeholder",
     github: "",
-    linkedIn: "linkedin.com/in/vigya-sharma-1540bb217/",
-    color: "#4b916c",
+    linkedIn: "",
+    color: "#3d649a",
+  },
+  {
+    name: "Subham Ghosh",
+    image: Placeholder,
+    role: "Outreach Lead",
+    description: "Placeholder",
+    github: "",
+    linkedIn: "",
+    color: "#3d649a",
+  },
+  {
+    name: "Janhavi Chaurasia",
+    image: Placeholder,
+    role: "Management Lead",
+    description: "Placeholder",
+    github: "",
+    linkedIn: "",
+    color: "#3d649a",
+  },
+  {
+    name: "Emmanuel Allan M J",
+    image: Placeholder,
+    role: "Web Development Lead",
+    description: "Placeholder",
+    github: "",
+    linkedIn: "",
+    color: "#3d649a",
+  },
+  {
+    name: "Swastika Shukla",
+    image: Placeholder,
+    role: "Blockchain Lead",
+    description: "Placeholder",
+    github: "",
+    linkedIn: "",
+    color: "#3d649a",
+  },
+  {
+    name: "Ameiy Acharya",
+    image: Placeholder,
+    role: "Blockchain Lead",
+    description: "Placeholder",
+    github: "",
+    linkedIn: "",
+    color: "#3d649a",
+  },
+  {
+    name: "Mihika Dhakray",
+    image: Placeholder,
+    role: "UI/UX Lead",
+    description: "Placeholder",
+    github: "",
+    linkedIn: "",
+    color: "#3d649a",
+  },
+  {
+    name: "Cynthia Konar",
+    image: Placeholder,
+    role: "App Development Lead",
+    description: "Placeholder",
+    github: "",
+    linkedIn: "",
+    color: "#3d649a",
   },
 ];
 
